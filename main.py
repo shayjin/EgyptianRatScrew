44ac6d3d24dc72f61539e9f6016f36e1884807d6
Jay Shin <83435667+shayjin@users.noreply.github.com>
2022-05-19 17:04:51 -0400

    big progress 😮‍💨

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import sys
 import random
 import time
 
-WIDTH = 700
-HEIGHT = 700
-
 one_h = pygame.image.load('hearts/1h.png')
 two_h = pygame.image.load('hearts/2h.png')
 three_h = pygame.image.load('hearts/3h.png')
@@ -74,19 +71,23 @@ pygame.mixer.init()
 set_sound = pygame.mixer.Sound('set.mp3')
 slap_sound = pygame.mixer.Sound('slap.wav')
 
-
 background_color = (100,120,120)
+WIDTH = 700
+HEIGHT = 700
+offset = 0
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Egyptian Rat Screw")
 screen.fill(background_color)
+font = pygame.font.Font('freesansbold.ttf',15)
 
 numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 shapes = ['Heart', 'Diamond', 'Spade', 'Clovers']
-
-
 attack_numbers = {'J':1, 'Q':2, 'K':3, 'A':4}
+
+
+
 
 class Card:
     number = ''
@@ -99,29 +100,27 @@ class Card:
         self.shape = shape
         self.is_attack_card = is_attack_card
         self.image = image
-        
+   
 
-
-        
-        
-deck = []
-for s in range(len(shapes)):
-    for n in range(len(numbers)):
-        is_attack_card = False
-        if numbers[n] in attack_numbers:
-            is_attack_card = True
-        else:
+def initialize(deck):
+    for s in range(len(shapes)):
+        for n in range(len(numbers)):
             is_attack_card = False
-        deck.append(Card(numbers[n], shapes[s], is_attack_card, card_images[s][n]))
-random.shuffle(deck)
+            if numbers[n] in attack_numbers:
+                is_attack_card = True
+            else:
+                is_attack_card = False
+            deck.append(Card(numbers[n], shapes[s], is_attack_card, card_images[s][n]))
+    return deck
 
-    
+deck = []
+deck = initialize(deck)
+random.shuffle(deck)
 
 player1_deck = []
 player2_deck = []
 player3_deck = []
-player4_deck = []
-        
+player4_deck = []    
         
 def distribute():
     i = 0
@@ -138,9 +137,7 @@ def distribute():
         i += 4
         
 distribute()
-    
-    
-                
+             
 class Player:
     name = ''
     deck = []
@@ -148,16 +145,46 @@ class Player:
     def __init__(self, name, deck):
         self.name = name
         self.deck = deck
-                
-    
+                 
 player1 = Player("User", player1_deck)
-player2 = Player("Andre Farinazo", player2_deck)
-player3 = Player("Luke Wingert", player3_deck)
-player4 = Player("Shay Jin", player4_deck)
+player2 = Player("Luke W", player2_deck)
+player3 = Player("Andre F", player3_deck)
+player4 = Player("Shay J", player4_deck)
     
 players = [player1, player2, player3, player4]
 
+USER_WIDTH = WIDTH / 7
+USER_HEIGHT = HEIGHT / 7
+user = pygame.image.load('user.jpeg')
+user = pygame.transform.scale(user, (USER_WIDTH, USER_HEIGHT)) 
+table_img = pygame.image.load('table.jpeg')
 turn = 1
+
+def init():
+    global screen
+    global table
+    global user
+    global turn
+    screen.blit(table_img, (WIDTH/2 - table_img.get_width()/2, HEIGHT/2 - table_img.get_height()/2))
+
+    screen.blit(user, (WIDTH/2-user.get_width()/2, HEIGHT-user.get_height()))
+    screen.blit(user, (WIDTH-user.get_width(), HEIGHT/2-user.get_height()/2))
+    screen.blit(user, (WIDTH/2-user.get_width()/2, 0))
+    screen.blit(user, (0, HEIGHT/2-user.get_height()/2))    
+
+    pygame.draw.circle(screen, (240,240,240),(WIDTH/2,HEIGHT), 150, 10)
+    pygame.draw.circle(screen, (240,240,240),(WIDTH,HEIGHT/2), 150, 10)
+    pygame.draw.circle(screen, (240,240,240),(WIDTH/2,0), 150, 10)
+    pygame.draw.circle(screen, (240,240,240),(0,HEIGHT/2), 150, 10)
+    if turn == 1:
+        pygame.draw.circle(screen, (215,185,230),(WIDTH/2,HEIGHT), 150, 10)
+    elif turn == 2:
+        pygame.draw.circle(screen, (215,185,230),(WIDTH,HEIGHT/2), 150, 10)
+    elif turn == 3:
+        pygame.draw.circle(screen, (215,185,230),(WIDTH/2,0), 150, 10)
+    elif turn == 4:
+        pygame.draw.circle(screen, (215,185,230),(0,HEIGHT/2), 150, 10) 
+    
 
 table = []
 
@@ -165,7 +192,6 @@ def status():
     print("1:")
     for x in player1_deck:
         print(x.number, x.shape)
-    
     print("2:")
     for x in player2_deck:
         print(x.number, x.shape)
@@ -176,22 +202,27 @@ def status():
     for x in player4_deck:
         print(x.number, x.shape)
     
-font = pygame.font.Font('freesansbold.ttf',15)
 
 def display_turn(card):
     global screen
     global font
 
-    if turn == 0:
+    if turn == 5:
         text = font.render("Player " + str(1), True, (128,0,0), (0,0,128))
     else:
         text = font.render("Player " + str(turn), True, (128,0,0), (0,0,128))
     screen.blit(text, (0,200))
     screen.blit(card.image, (WIDTH/4+offset, HEIGHT/3+WIDTH/8))
+    set_sound.play()
     pygame.display.update()
+    time.sleep(0.8)
     
 def player_turn(turn, next_or_prev):
     global offset
+    pygame.draw.circle(screen, (240,240,240),(WIDTH/2,HEIGHT), 150, 10)
+    pygame.draw.circle(screen, (240,240,240),(WIDTH,HEIGHT/2), 150, 10)
+    pygame.draw.circle(screen, (240,240,240),(WIDTH/2,0), 150, 10)
+    pygame.draw.circle(screen, (240,240,240),(0,HEIGHT/2), 150, 10)
     if next_or_prev == 'prev':
         turn -= 1
                         
@@ -202,27 +233,63 @@ def player_turn(turn, next_or_prev):
             print("player " + str(turn) + " ran out of cards!")
             turn = player_turn(turn, 'prev')
     elif next_or_prev == 'next':
-        turn += 1
-        if turn == 5:
-            turn = 1
+        
+        print(turn)
         if len(players[turn-1].deck) == 0:
             print("player " + str(turn) + " ran out of cards!")
             turn = player_turn(turn, 'next')
+        turn += 1
+        if turn == 5:
+            turn = 1
+
+        if turn == 1:
+            pygame.draw.circle(screen, (215,185,230),(WIDTH/2,HEIGHT), 150, 10)
+        elif turn == 2:
+            pygame.draw.circle(screen, (215,185,230),(WIDTH,HEIGHT/2), 150, 10)
+        elif turn == 3:
+            pygame.draw.circle(screen, (215,185,230),(WIDTH/2,0), 150, 10)
+        elif turn == 4:
+            pygame.draw.circle(screen, (215,185,230),(0,HEIGHT/2), 150, 10)
+            
     return turn
     
         
 def lost_round_msg(turn):
     global offset
     global font
-    if turn == 4:
-        text = font.render("Player " + str(1) + " lost the round", True, (128,0,0), (0,0,128))
-    else:
-        text = font.render("Player " + str(turn+1) + " lost the round", True, (128,0,0), (0,0,128))
-    screen.blit(text, (100,100))
-    text = font.render('Player ' + str(turn) + '\'s Turn', True, (128,0,0), (0,0,128))
-    screen.blit(text, (100,200))
+    text = font.render("Player " + str(turn) + " won the round", True, (0,0,0), background_color)
+    screen.blit(text, (WIDTH/2-WIDTH/7, 200))
     offset = 0
-    pygame.display.update()
+    for i in range(10):
+        if turn == 1:
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (240,240,240),(WIDTH/2,HEIGHT), 150, 10)
+            pygame.display.update()
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (134,50,20),(WIDTH/2,HEIGHT), 150, 10)
+            pygame.display.update() 
+        if turn == 2:
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (240,240,240),(WIDTH,HEIGHT/2), 150, 10)
+            pygame.display.update()
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (134,50,20),(WIDTH,HEIGHT/2), 150, 10)
+            pygame.display.update() 
+        if turn == 3:
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (240,240,240),(WIDTH/2,0), 150, 10)
+            pygame.display.update()
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (134,50,20),(WIDTH/2,0), 150, 10)
+            pygame.display.update() 
+        if turn == 4:
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (240,240,240),(0,HEIGHT/2), 150, 10)
+            pygame.display.update()
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (134,50,20),(0,HEIGHT/2), 150, 10)
+            pygame.display.update()    
+    time.sleep(1.5)
     print('player 1: ' + str(len(player1.deck)))
     print('player 2: ' + str(len(player2.deck)))
     print('player 3: ' + str(len(player3.deck)))
@@ -232,10 +299,8 @@ def slap_lost_round_msg(turn):
     global font
     global offset 
     offset = 0
-    text = font.render("Player " + str(turn) + " takes all the card", True, (128,0,0), (0,0,128))
-    screen.blit(text, (100,100))
-    text = font.render('Player ' + str(turn) + '\'s Turn', True, (128,0,0), (0,0,128))
-    screen.blit(text, (100,200))
+    text = font.render("Player " + str(turn) + " takes all the card", True, (0,0,0), background_color)
+    screen.blit(text, (WIDTH/2-WIDTH/7, 200))
     pygame.display.update()
     print('player 1: ' + str(len(player1.deck)))
     print('player 2: ' + str(len(player2.deck)))
@@ -243,12 +308,12 @@ def slap_lost_round_msg(turn):
     print('player 4: ' + str(len(player4.deck)))
 
 slap = False
+
 def draw(player):
     global offset
     global table
     global slap
     global turn
-    time.sleep(0.3)
     card_drawn = player.deck.pop()
     table.append(card_drawn)
     offset += 15
@@ -259,15 +324,15 @@ def draw(player):
         slap = True
         turn = 5
         
-    set_sound.play()
-    text = font.render(str(len(player1.deck)) + " cards", True, (128,0,0), (0,0,128))
-    screen.blit(text, (WIDTH/2,HEIGHT-30))
-    text = font.render(str(len(player2.deck)) + " cards", True, (128,0,0), (0,0,128))
-    screen.blit(text, (WIDTH-30,HEIGHT/2))
-    text = font.render(str(len(player3.deck)) + " cards", True, (128,0,0), (0,0,128))
-    screen.blit(text, (WIDTH/2,0))
-    text = font.render(str(len(player4.deck)) + " cards", True, (128,0,0), (0,0,128))
-    screen.blit(text, (0,HEIGHT/2))
+    text = font.render(str(len(player1.deck)) + " cards", True, (128,0,0), (100,120,128))
+    screen.blit(text, (WIDTH/2-30,HEIGHT-15))
+    text = font.render(str(len(player2.deck)) + " cards", True, (128,0,0), (100,120,128))
+    screen.blit(text, (WIDTH-80,HEIGHT/2+50))
+    text = font.render(str(len(player3.deck)) + " cards", True, (128,0,0), (100,120,128))
+    screen.blit(text, (WIDTH/2-30,USER_HEIGHT))
+    text = font.render(str(len(player4.deck)) + " cards", True, (128,0,0), (100,120,128))
+    screen.blit(text, (20,HEIGHT/2+50))
+    time.sleep(0.1)
     return card_drawn
     
 def whose_card(turn):
@@ -281,6 +346,7 @@ def reset():
     table = []
     time.sleep(1)
     screen.fill(background_color)
+    init()
     pygame.display.update()
 
 player1_chances = 0
@@ -289,11 +355,14 @@ def player1_defense():
     global player1_chances
     global turn
     global table
+    global offset
     if len(player1.deck) > 0:
         if is_slap(table):
             turn = 5
         else:
             card_drawn = draw(player1)
+            screen.blit(card_drawn.image, (WIDTH/2-WIDTH/4 + offset, HEIGHT/3+WIDTH/8))
+            pygame.display.update()
             if is_slap(table):
                 turn = 5
             else:
@@ -304,12 +373,20 @@ def player1_defense():
                     if player1_chances == 0:
                         for card in table:
                             players[3].deck.append(card)
-                        lost_round_msg(1)
+                        lost_round_msg(4)
                         reset()
                         turn = 4
                 elif player1_chances > 0:
                     display_turn(card_drawn)
                     turn = 2
+                else: 
+                    turn = player_turn(turn, 'prev')
+                            
+                    for card in table:
+                        players[turn-1].deck.append(card)
+            
+                    lost_round_msg(turn)
+                    reset()
             
 def is_slap(table):
     if len(table) == 2:
@@ -345,6 +422,13 @@ def should_slap(player):
         turn = 2
         slap = False
         slap_lost_round_msg(turn)
+        for i in range(10):
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (240,240,240),(WIDTH,HEIGHT/2), 150, 10)
+            pygame.display.update()
+            time.sleep(0.05)
+            pygame.draw.circle(screen, (134,50,20),(WIDTH,HEIGHT/2), 150, 10)
+            pygame.display.update()
         reset()
         x = 0
         return 0
@@ -362,8 +446,6 @@ def should_slap(player):
             x = 0
             return 0
             
-        
-offset = 0
     
 def take_turn(player):
     global turn
@@ -417,18 +499,19 @@ def take_turn(player):
             else:
                 card_drawn = draw(player)
                 whose_card(turn)
-                screen.blit(card_drawn.image, (WIDTH/2-WIDTH/4 + offset, HEIGHT/3+WIDTH/8))
-                if not slap:
-                    turn = player_turn(turn, 'next')
-                else:
+                display_turn(card_drawn)
+                if slap:
                     turn = 5
+                else:
+                    turn = player_turn(turn, 'next')
         else:
             card_drawn = draw(player)
             whose_card(turn)
-            screen.blit(card_drawn.image, (WIDTH/2-WIDTH/4 + offset, HEIGHT/3+WIDTH/8))
+            display_turn(card_drawn)
             turn = player_turn(turn, 'next')
     pygame.display.update()
-
+    
+init()
 while True:
     if turn > 1:
         if slap:
@@ -436,6 +519,7 @@ while True:
             x += 1
             print(x)
         else: 
+            print('lel' + str(turn))
             take_turn(players[turn-1])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
